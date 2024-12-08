a5d4e849830c57846992d0472156179116967672
sukizumi <187249056+sukizumi@users.noreply.github.com>
2024-12-08 03:52:43 +0000

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  root to: 'homes#top'
+  #get '/top' => 'homes#top'
+  resources :books
+end
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #get 'books/new'
+  #post 'books' => 'books#create'
+  #get 'books' => 'books#index'
+  #get 'books/:id' => 'books#show', as: 'book'
+  #get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  #patch 'books/:id' => 'books#update', as: 'update_book'
+  #delete 'books/:id' => 'books#destroy', as: 'destroy_book'
